266cce0a852cf5ca1197864be2781d10a688325a
thirteenfoil8 <florian.genilloud@gmail.com>
2015-01-06 08:04:29 +0100

    Mise en place du bouton d'ajout qui ne marche pas

--- script-ex.js
+++ script-ex.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-
+    $("#aAjouter").click(function() {
+        var formule = $("#ajouter").val();
+        $("#Exercice").append("<p>"+ formule +"</p>");
+    });
 });
